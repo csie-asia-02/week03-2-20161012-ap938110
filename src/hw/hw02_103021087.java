b3a767cbfb5f7f3a83da01966c1f602766d8a081
ap938110 <ap938110@gmail.com>
2017-01-03 21:52:48 +0800

    103021087 張問鼎

---
+++ src/hw/hw02_103021087.java
@@ -0,0 +1,20 @@
+package hw;
+
+import java.util.Scanner;
+public class hw02_103021087 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		System.out.println("性別(1:男生,2:女生):");
+		int genda=scn.nextInt();
+		System.out.print("身高");
+		int h = scn.nextInt();
+		if(genda==1){
+			System.out.println("體重為"+((h-80)*0.7));	
+		}else{
+	    if(genda==2){
+	    	System.out.println("體重為"+((h-70)*0.6));
+	    }else{
+	    	System.out.println("性別錯誤");
+}}}}
