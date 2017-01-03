2440011824090068ed7b8764051f64462a1716b5
ap938110 <ap938110@gmail.com>
2017-01-03 23:54:10 +0800

    103021087 張問鼎

--- src/hw/hw01_103021087.java
+++ src/hw/hw01_103021087.java
@@ -5,7 +5,7 @@ public class hw01_103021087 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("請輸入每月通畫時間(分鐘)");
+		System.out.println("每月通畫時間?(分鐘)");
 		Scanner scn = new Scanner(System.in);
 		int v1 = scn.nextInt();
 		if (v1 < 600) {
@@ -14,4 +14,6 @@ public class hw01_103021087 {
 			System.out.println("電話費(元))" + (v1 * 05 * 0.9));
 		} else if (v1 > 1200) {
 			System.out.println("電話費(元))" + (v1 * 05 * 0.79));
-		}}}
+		}
+	}
+}
